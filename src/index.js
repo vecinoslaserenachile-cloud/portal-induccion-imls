ef66bc627f0ad00dcf927b7b5ece074519b9aaf4
vecinoslaserenachile-cloud <vecinoslaserenachile@gmail.com>
2026-02-27 23:48:48 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // <--- ESTO ES VITAL: Importa los estilos
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
