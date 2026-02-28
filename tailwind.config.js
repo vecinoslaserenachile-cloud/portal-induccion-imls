187252f5c82e412a122c4f99c97a70c92166ddc8
vecinoslaserenachile-cloud <vecinoslaserenachile@gmail.com>
2026-02-27 22:14:25 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Esto le dice: "Busca estilos en todos mis archivos React"
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
